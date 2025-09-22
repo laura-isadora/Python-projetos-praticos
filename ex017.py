569454d334710fa13325ec91f7cb4e96c1c1cb01
Laura Isadora <laura.moreira17@hotmail.com>
2025-09-22 16:13:13 -0300

    Update ex017.py

--- ex017.py
+++ ex017.py
@@ -9,6 +9,15 @@ r3 = float(input('Digite o terceiro segmento: '))
 
 # calculo para saber se pode ser formado um triângulo
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r2 + r1:
-    print('Esses segmentos formam um triângulo!')
+     # três retas iguais formam um triângulo equilátero
+    if r1 == r2 == r3:
+        print('Esses segmentos formam um triângulo EQUILÁTERO!')
+    # duas retas iguais e uma diferente formam um triângulo isósceles
+    elif r1 == r2 or r2 == r3 or r3 == r1:
+        print('Esses segmentos formam um triângulo ISÓSCELES!')
+    # todas as retas com comprimentos diferentes formam um triângulo escaleno
+    else:
+        print('Esses segmentos formam um triângulo ESCALENO!')
 else:
     print('Esses segmentos NÃO podem formar um triângulo!')
+
